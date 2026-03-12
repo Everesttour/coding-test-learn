532a10ac780b9016002d3d9307d9c93bc93ba7ce
EverestTour <everestimpact1202@gmail.com>
2026-03-12 16:18:04 +0900

    Refactor solution function to correctly sum play counts and enhance code documentation

--- programmers/베스트앨범42579.java
+++ programmers/베스트앨범42579.java
@@ -1,5 +1,39 @@
 import java.util.*;
 
+/*
+# 사용된 함수
+HashMap.getOrDefault(key, defaultValue)
+정의: HashMap에서 key 값을 가져오고, 없으면 defaultValue를 반환
+
+HashMap.containsKey(key)
+정의: HashMap에 특정 key가 존재하는지 확인
+
+HashMap.keySet()
+정의: HashMap의 모든 key를 Set 형태로 반환
+
+List.sort(Comparator)
+정의: List를 비교 기준(Comparator)에 따라 정렬
+
+Integer.compare(x, y)
+정의: Integer 클래스의 정수 비교 함수
+
+이외
+Collection.stream()
+IntStream mapToInt(ToIntFunction)
+Integer::intValue
+IntStream.toArray()
+*/
+
+
+/*
+# 문제 내용
+스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다. v
+속한 노래가 많이 재생된 장르를 먼저 수록합니다. v
+장르 내에서 많이 재생된 노래를 먼저 수록합니다. v
+장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다. v
+노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.v
+*/
+
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         
@@ -8,7 +42,7 @@ class Solution {
         Map<String, List<Integer>> genresSongList = new HashMap<>();
         
         for(int i=0; i<genres.length; i++) {
-            genresPlaySum.put(genres[i], genresPlaySum.getOrDefault(genres[i], 0) + 0);
+            genresPlaySum.put(genres[i], genresPlaySum.getOrDefault(genres[i], 0) + plays[i]);
             
             if(!genresSongList.containsKey(genres[i])) {
                 genresSongList.put(genres[i], new ArrayList<>());
@@ -44,9 +78,3 @@ class Solution {
     }
 }
 
-//스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다. v
-
-//속한 노래가 많이 재생된 장르를 먼저 수록합니다. v
-//장르 내에서 많이 재생된 노래를 먼저 수록합니다. v
-//장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다. v
-//노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.v
